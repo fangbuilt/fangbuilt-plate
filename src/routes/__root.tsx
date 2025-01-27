819e6b46ae576db0fc085dbdc6e91ab932efa34c
Bintang <1bintangnaufal@gmail.com>
2025-01-27 10:42:27 +0700

    init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,43 @@
+import type { QueryClient } from "@tanstack/react-query";
+import {
+  createRootRouteWithContext,
+  Link,
+  Outlet,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
+  {
+    component: RootComponent,
+    notFoundComponent: NotFound,
+  },
+);
+
+function RootComponent() {
+  return (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+      <ReactQueryDevtools />
+    </>
+  );
+}
+
+function NotFound() {
+  return (
+    <>
+      <p>This is the not found component configured on root route</p>
+      <Link to="/">Start Over</Link>
+    </>
+  );
+}
---
+++ src/routes/about.lazy.tsx
@@ -0,0 +1,14 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/about")({
+  component: About,
+});
+
+function About() {
+  return (
+  <div className="p-2">
+    <p>Hello from about!</p>
+    <button type="button">Button Appears Like This</button>
+    </div>
+);
+}
