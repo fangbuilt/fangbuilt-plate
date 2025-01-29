21c39028c548225f6a49063b6383d749da072e45
Bintang <1bintangnaufal@gmail.com>
2025-01-29 22:49:12 +0700

    removed react compiler

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import pluginQuery from '@tanstack/eslint-plugin-query'
 import pluginRouter from '@tanstack/eslint-plugin-router'
-import reactCompiler from 'eslint-plugin-react-compiler'
+// import reactCompiler from 'eslint-plugin-react-compiler'
 
 export default tseslint.config(
   { ignores: ['dist', '**/routeTree.gen.ts'] },
@@ -26,14 +26,14 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
       '@tanstack/query': pluginQuery,
       '@tanstack/router': pluginRouter,
-      'react-compiler': reactCompiler,
+      // 'react-compiler': reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@tanstack/query/exhaustive-deps': 'error',
       '@tanstack/router/create-route-property-order': 'error',
-      'react-compiler/react-compiler': 'error',
+      // 'react-compiler/react-compiler': 'error',
     },
   }
 )
