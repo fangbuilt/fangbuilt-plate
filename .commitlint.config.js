53bdf10baa0f6715f613554b0c5f98a0e0ab3961
Bintang <1bintangnaufal@gmail.com>
2025-01-30 13:13:49 +0700

    invalid

--- .commitlint.config.js
+++ .commitlint.config.js
@@ -1,6 +1,6 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-  // rules: {
-  //   'type-enum': [2, ['always', 'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test']],
-  // },
+  rules: {
+    'type-enum': [2, ['always', 'build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test']],
+  },
 }
